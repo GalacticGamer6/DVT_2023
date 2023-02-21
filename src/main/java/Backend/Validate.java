1e7fdb9919b7374aea2fbe82b2a2c6d5f2dede0f
GalacticGamer6 <nereaav.ranjit@reddam.house>
2023-02-21 12:37:14 +0200

    kk

--- src/main/java/Backend/Validate.java
+++ src/main/java/Backend/Validate.java
@@ -2,6 +2,40 @@ package Backend;
 
 public class Validate {
     
+    public static void main(String[] args) {
+
+        validateSpeciesName("Pi%kachu");
+        
+    }
+    
+    public static boolean validateSpeciesName(String species_name){
+        
+        char [] arr  = species_name.toCharArray();
+        for(int i = 0; i < arr.length;i++){
+            
+            if(Character.isDigit(arr[i])){
+                System.out.println("Pokemon name cannot contain a number");
+                return false;
+                
+            }
+            else if(Character.isSpaceChar(arr[i])){
+                System.out.println("Pokemon name cannot contain a space");
+                return false;
+            }
+            else if(!Character.isAlphabetic(arr[i])){
+                System.out.println("Pokemon name cannot contain a special Character");
+                return false;
+            }
+              
+        }
+        //cant have numbers in name
+        //cannot have any special characters in name
+        //cannot have whitespace in name
+        
+        return true;
+    }
+    
+    //need a height and weight check
     
     
 }
